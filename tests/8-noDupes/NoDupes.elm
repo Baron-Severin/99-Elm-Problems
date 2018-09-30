d839363d2089eb1ad471ca85013f492f70d92f0c
THOR\Severin <severinrudie@gmail.com>
2018-09-29 20:26:37 -0700

    #8 noDupes

---
+++ tests/8-noDupes/NoDupes.elm
@@ -0,0 +1,49 @@
+module NoDupes exposing (..)
+
+import Debug exposing (toString)
+import Html
+import List exposing (head, tail)
+import Maybe
+
+
+func : a -> List a -> List a
+func value acc =
+    let
+        first = head acc
+    in
+        case first of
+            Nothing -> [value]
+            Just f -> if f == value then acc
+                        else [value] ++ acc
+
+noDupes : List a -> List a
+noDupes xs =
+    List.foldr func [] xs
+
+
+main : Html.Html a
+main =
+    Html.text
+        <| case test of
+            0 ->
+                "Your implementation passed all tests."
+
+            1 ->
+                "Your implementation failed one test."
+
+            x ->
+                "Your implementation failed " ++ (toString x) ++ " tests."
+
+
+test : Int
+test =
+    List.length
+        <| List.filter ((==) False)
+            [ noDupes [ 1, 1, 1, 1, 2, 5, 5, 2, 1 ] == [ 1, 2, 5, 2, 1 ]
+            , noDupes [ 2, 1, 1, 1 ] == [ 2, 1 ]
+            , noDupes [ 2, 2, 2, 1, 1, 1 ] == [ 2, 1 ]
+            , noDupes [ 1 ] == [ 1 ]
+            , noDupes [] == []
+            , noDupes [ "aa", "aa", "aa" ] == [ "aa" ]
+            , noDupes [ "aab", "b", "b", "aa" ] == [ "aab", "b", "aa" ]
+            ]
