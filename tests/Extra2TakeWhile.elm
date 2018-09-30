be8442ec0cb49a2e833e2fd8ac6b1588da2bcc45
THOR\Severin <severinrudie@gmail.com>
2018-09-30 02:06:58 -0700

    Refactored Extra 2 takeWhile

--- tests/Extra2TakeWhile.elm
+++ tests/Extra2TakeWhile.elm
@@ -1,9 +1,11 @@
 module Extra2TakeWhile exposing (..)
 
 import Debug exposing (toString)
+import Expect
 import Html exposing (text)
 import List exposing (head, tail)
 import Basics exposing (remainderBy)
+import Test exposing (Test, test, describe)
 
 
 headPasses : (a -> Bool) -> List a -> Bool
@@ -22,40 +24,31 @@ tailOrEmpty list =
 
 takeWhile : (a -> Bool) -> List a -> List a
 takeWhile predicate list =
-    let
-        first = head list
-    in
-        case first of
-            Nothing -> []
-            Just value -> if ( predicate value ) then ( List.singleton value ) ++ ( takeWhile predicate ( tailOrEmpty list ) )
-                            else []
-
-
-main : Html.Html a
-main =
-    Html.text
-        <| case suite of
-            0 ->
-                "Your implementation passed all tests."
-
-            1 ->
-                "Your implementation failed one test."
-
-            x ->
-                "Your implementation failed " ++ (toString x) ++ " tests."
-
-
-suite : Int
-suite =
-    List.length
-        <| List.filter ((==) False)
-          [ (takeWhile isOdd [ 1, 2, 1 ] == [ 1 ])
-          , (takeWhile isEven [ 1, 2, 1 ] == [])
-          , (takeWhile isEven [] == [])
-          , (takeWhile isEven [ 2, 4, 100000, 1 ] == [ 2, 4, 100000 ])
-          , (takeWhile ((>) 5) (List.range 1 10) == [ 1, 2, 3, 4 ])
-          , (takeWhile ((>) 50) (List.range 1 10) == List.range 1 10)
-          ]
+    case head list of
+        Nothing -> []
+        Just value -> if (predicate value) == False then []
+                      else
+                        let
+                            theRest = takeWhile predicate ( tailOrEmpty list )
+                        in
+                             value :: theRest
+          
+suite : Test
+suite = describe "pre-written"
+    [ test "1" <|
+              \_ -> takeWhile isOdd [ 1, 2, 1 ] |> Expect.equal [ 1 ]
+    ,  test "2" <|
+              \_ -> takeWhile isEven [ 1, 2, 1 ] |> Expect.equal []
+    ,  test "3" <|
+              \_ -> takeWhile isEven [] |> Expect.equal []
+    ,  test "4" <|
+              \_ -> takeWhile isEven [ 2, 4, 100000, 1 ] |> Expect.equal [ 2, 4, 100000 ]
+    ,  test "5" <|
+              \_ -> takeWhile ((>) 5) (List.range 1 10) |> Expect.equal [ 1, 2, 3, 4 ]
+    ,  test "6" <|
+              \_ -> takeWhile ((>) 50) (List.range 1 10) |> Expect.equal ( List.range 1 10 )
+    ]
+          
 
 isEven : Int -> Bool
 isEven x =
